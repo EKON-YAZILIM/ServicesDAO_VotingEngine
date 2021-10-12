503f1aaa763511208e74ad4f92b7679a06e87325
yalcinyasin14@gmail.com <yalcinyasin14@gmail.com>
2021-10-12 16:28:41 +0300

    Helpers Cleanup.

--- Helpers/Models/DtoModels/VoteDbDto/UserReputationDto.cs
+++ Helpers/Models/DtoModels/VoteDbDto/UserReputationDto.cs
@@ -4,17 +4,11 @@ using System.Runtime.Serialization;
 
 namespace Helpers.Models.DtoModels.VoteDbDto
 {
-    [DataContract]
     [Serializable]
     public partial class UserReputationDto
     {
-        [DataMember]
         public int UserReputationID { get; set; }
-
-        [DataMember]
         public int UserId { get; set; }
-
-        [DataMember]
         public double Reputation { get; set; }
     }
 }
--- Helpers/Models/DtoModels/VoteDbDto/VoteDto.cs
+++ Helpers/Models/DtoModels/VoteDbDto/VoteDto.cs
@@ -5,23 +5,13 @@ using System.Runtime.Serialization;
 
 namespace Helpers.Models.DtoModels.VoteDbDto
 {
-    [DataContract]
     [Serializable]
     public partial class VoteDto
     {
-        [DataMember]
         public int VoteId { get; set; }
-
-        [DataMember]
         public int VoteJobID { get; set; }
-
-        [DataMember]
         public double Reputation { get; set; }
-
-        [DataMember]
         public string Side { get; set; }
-
-        [DataMember]
         public DateTime Date { get; set; }
     }
 }
