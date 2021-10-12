824fb2145b316df5643e0d496ab6e63aaacb6018
Ekin Keçeci <ekin573@hotmail.com>
2021-10-12 15:21:37 +0300

    Project infastructure

---
+++ Helpers/Models/IdentityModels/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.Models.IdentityModels
+{
+    public class RegisterModel
+    {
+        public string email { get; set; }     
+        public string namesurname { get; set; }
+        public string gsm { get; set; }
+        public string password { get; set; }
+        public string registerEmailTitle { get; set; }
+        public string registerEmailContent { get; set; }
+        public string ip { get; set; }
+        public string port { get; set; }
+    }
+}
